4f468b8bc8910ed788020c1731645ea1df9bcfef
Chen-Z-H <31105348+Chen-Z-H@users.noreply.github.com>
2018-05-16 10:08:10 +0200

    Add files via upload

--- elasticioEx.py
+++ elasticioEx.py
@@ -8,6 +8,7 @@ searcharticleurl = "/articles/_search?"
 searchuserurl = "/users/_search?"
 posturl = "localhost/articles/_doc"
 NUM_QUERY_HISTORY = 10
+NUM_CLICK_HISTORY = 50
 NUM_ARTICLES = 20
 
 
@@ -140,7 +141,7 @@ def getUserPreferences(id):
     return out
 
 
-def addQueryHistory(id, query):
+def addQueryHistory(userID, query, docid=""):
     # Args:
     # id: User ID
     # query: User query
@@ -153,31 +154,47 @@ def addQueryHistory(id, query):
     # void
     # For every string in the list of categories, the corresponding user category bin is incremented by one.
     conn = hc.HTTPConnection(server)
-    conn.request("GET", searchuserurl + "q=id:" + (str)(id) + "&_source=search_history")
+    conn.request("GET", searchuserurl + "q=id:" + (str)(userID) + "&_source=search_history")
     response = conn.getresponse()
     data = json.loads(response.read())
     if (data["hits"]["total"] != 1):
-        print("Error: user with id %d exists %d times" % (id, data["hits"]["total"]))
+        print("Error: user with id %d exists %d times" % (userID, data["hits"]["total"]))
         conn.close()
         return 0
     user = data["hits"]["hits"][0]["_source"]
-    print(user)
+    # print(user)
+
     history = user["search_history"]
-    print(history)
-    index = len(history)
-    if len(history) == NUM_QUERY_HISTORY:
-        for i in range(0, NUM_QUERY_HISTORY - 1):
-            history[i] = history[i + 1]
-        del (history[-1])
-    history.append(query)
-    print(history)
-    url = ("/users/user/%d/_update" % (id))
+    print((str)(history) + " in addQueryHistory()")
+    if query in history:
+        clickthrough = history[query]
+        if docid != "":
+            if len(clickthrough) == NUM_CLICK_HISTORY:
+                for i in range(0, NUM_CLICK_HISTORY - 1):
+                    clickthrough[i] = clickthrough[i + 1]
+                del (clickthrough[-1])
+            clickthrough.append(docid)
+    else:
+        history[query] = []
+
+    print((str)(history) + " in addQueryHistory() after added")
+    # history = list(history.items())     # turn the dictionary to list
+    # index = len(history)
+    # if len(history) == NUM_QUERY_HISTORY:
+    #     for i in range(0, NUM_QUERY_HISTORY - 1):
+    #         history[i] = history[i + 1]
+    #     del (history[-1])
+    # history.append(query)
+    # history = dict(history)             # turn the list to dictionary
+    # print(history)
+    url = ("/users/user/%d/_update" % (userID))
     newdata = {
         "doc": {
             "search_history": history
         }
     }
-    params = json.dumps(newdata);
+
+    params = json.dumps(newdata)
     conn.request("POST", url, params, {"Content-type": "application/json"})
     response = json.loads(conn.getresponse().read())
     conn.close()
@@ -185,6 +202,47 @@ def addQueryHistory(id, query):
         return 1
     else:
         return 0
+
+def getUserHistory(userID):
+    conn = hc.HTTPConnection(server)
+    conn.request("GET", searchuserurl + "q=id:" + (str)(userID) + "&_source=search_history")
+    response = conn.getresponse()
+    data = json.loads(response.read())
+    print(data)
+    if (data["hits"]["total"] != 1):
+        print("Error: user with id %d exists %d times" % (userID, data["hits"]["total"]))
+        conn.close()
+        return 0
+    user = data["hits"]["hits"][0]["_source"]
+    conn.close()
+    print((str)(user['search_history']) + " in getUserHistory()")
+    return user['search_history']
+
+def getTermVectors(docId):
+    '''
+    param: docId
+    return: dictionary with terms as keys and values are a dictionary of the form:
+    {
+        "term_freq": tf,
+        "tokens": [{
+            "position": 80,
+            "start_offset": 538,
+            "end_offset": 539
+        },...]
+    }
+    '''
+    conn = hc.HTTPConnection(server)
+    new_data = {
+        "fields":["text"]
+    }
+    params = json.dumps(new_data)
+    conn.request("POST", '/articles/article/'+docId+'/_termvectors', params, \
+                 {"Content-type": "application/json"})
+    response = conn.getresponse()
+    data = json.loads(response.read())
+    terms = data['term_vectors']['text']['terms']
+    conn.close()
+    return terms
 
 
 def main():
